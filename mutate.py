4d195a74d786c17255b374c56ce24607eab4af99
Joshua Nahum <josh@nahum.us>
2012-06-14 14:14:09 -0700

    added bitstring to mutate options

--- mutate.py
+++ mutate.py
@@ -1,4 +1,5 @@
 import random
+import bitstring
 
 random_generator = random.Random()
 
@@ -6,6 +7,8 @@ random_generator = random.Random()
 def mutate_value(value):
     if isinstance(value, int):
         return shift_by_one(value)
+    elif isinstance(value, bitstring.Bitstring):
+        return flip_single_bit(value)
     else:
         raise TypeError("unknown type to mutate")
 
@@ -15,3 +18,8 @@ def shift_by_one(number):
         return number + 1
     else:
         return number - 1
+
+
+def flip_single_bit(bitstring_):
+    position = random_generator.randrange(len(bitstring_))
+    return bitstring.flip_positions(bitstring_, [position])
--- test_mutate.py
+++ test_mutate.py
@@ -1,5 +1,6 @@
 from unittest import TestCase as TC
 import mutate
+import bitstring
 
 
 class MockRandom(object):
@@ -7,6 +8,9 @@ class MockRandom(object):
         self.value = value
 
     def random(self):
+        return self.value
+
+    def randrange(self, stop):
         return self.value
 
 
@@ -21,6 +25,15 @@ class TestModule(TC):
 
         self.assertSetEqual({mutated_value0, mutated_value1},
                 {value + 1, value - 1})
+
+    def test_mutate_bitstring(self):
+        value = bitstring.Bitstring("00")
+        mutated_value = mutate.mutate_value(value)
+        self.assertNotEqual(value, mutated_value)
+
+        mutate.random_generator = MockRandom(1)
+        mutated_value_ = mutate.mutate_value(value)
+        self.assertEqual(bitstring.Bitstring("01"), mutated_value_)
 
     def test_mutate_value_raises(self):
         with self.assertRaises(TypeError):
