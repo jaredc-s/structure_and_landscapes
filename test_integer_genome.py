821b73004dac2ed0b59e39d3eb0edf0b19fccc77
Joshua Nahum <josh@nahum.us>
2012-06-14 12:18:45 -0700

    added default genome to integer genome

--- test_integer_genome.py
+++ test_integer_genome.py
@@ -2,5 +2,10 @@ from unittest import TestCase as TC
 
 import integer_genome
 
+
 class TestGenome(TC):
     pass
+
+class TestModule(TC):
+    def test_default_genome(self):
+        genome = integer_genome.default_genome
