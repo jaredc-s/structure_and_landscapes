73b2229523ca0b9f918c5fafa068698608b36d13
Joshua Nahum <josh@nahum.us>
2012-06-14 12:28:14 -0700

    added tests for integer genome

--- integer_genome.py
+++ integer_genome.py
@@ -4,6 +4,7 @@ Simple representation of a genome.
 Intended for testing purposes.
 """
 
+
 class Genome(object):
     def __init__(self, value):
         assert(isinstance(value, int))
--- test_integer_genome.py
+++ test_integer_genome.py
@@ -1,10 +1,22 @@
+import unittest
 from unittest import TestCase as TC
 
 import integer_genome
+from integer_genome import Genome
 
 
 class TestGenome(TC):
-    pass
+    def test_init(self):
+        genome = Genome(0)
+        self.assertEqual(genome.value, 0)
+
+        genome2 = Genome(-4)
+        self.assertEqual(genome2.value, -4)
+
+    def test_init_exception(self):
+        with self.assertRaises(AssertionError):
+            Genome("1")
+
 
 class TestModule(TC):
     def test_default_genome(self):
