2c69c75ae9636c0927efc854e246d48b2debb73f
Jared Carlson-Stevermer <jmcs@utexas.edu>
2012-06-19 15:59:24 -0700

    added basic plotting function to supplement main.py

---
+++ basic_plot.py
@@ -0,0 +1,16 @@
+from numpy import *
+from pylab import *
+from matplotlib import *
+from scipy import *
+
+
+def plot_average_fitness(fitness):
+    values = [average_gen(gen) for gen in fitness]
+
+    xlabel("Updates")
+    ylabel("Average Fitness")
+    plot(values)
+    show()
+
+def average_gen(generation_fitness):
+    return average(generation_fitness)
