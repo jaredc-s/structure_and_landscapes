50344ce2d9d33afcc4dc2eb8e968558b2d5d83da
Joseph Marcus <josephhmarcus@gmail.com>
2012-06-19 17:33:51 -0700

    added docstrings to bitstring_organism.py and simplified the fitness function

--- bitstring_organism.py
+++ bitstring_organism.py
@@ -11,10 +11,17 @@ from bitstring import Bitstring
 
 class Organism(object):
     def __init__(self, value):
+        """
+        init takes a single argument which should be a bit string
+        """
         assert(isinstance(value, Bitstring))
         self.value = value
 
     def __eq__(self, other):
+        """
+        organisms are equal if they have the same type
+        and have the same bitstring
+        """
         if not isinstance(other, type(self)):
             return False
         return self.value == other.value
@@ -23,15 +30,26 @@ class Organism(object):
         return not self == other
 
     def __hash__(self):
+        """
+        organisms are hashable
+        """
         return hash(self.value)
 
     def mutate(self):
+        """
+        the mutate method of an organism calls the module
+        mutate and returns a new organism with the mutation
+        note: original organism is unchanged
+        """
         mutated_value = mutate_value(self.value)
         return Organism(mutated_value)
 
     @property
     def fitness(self):
-        all_zeroes = Bitstring(False for _ in self.value)
-        return float(all_zeroes.hamming_distance(self.value))
+        """
+        fitness of this organism is the hamming distance of
+        its bitstring from a bitstring composed of all False's
+        """
+        return sum(self.value)
 
 default_organism = Organism(Bitstring(False for _ in range(10)))
