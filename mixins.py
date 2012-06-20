956e9a4ab8817ebb1344728aa48319b23364655d
Joshua Nahum <josh@nahum.us>
2012-06-20 13:33:21 -0700

    fixed test class name and pep8

--- mixins.py
+++ mixins.py
@@ -1,17 +1,20 @@
 import functools
 
+
 class KeyedEqualityMixin(object):
-   def __eq__(self, other):
-       return self.__key__() == other.__key__()
-   def __ne__(self, other):
-       return self.__key__() != other.__key__()
+    def __eq__(self, other):
+        return self.__key__() == other.__key__()
+
+    def __ne__(self, other):
+        return self.__key__() != other.__key__()
+
 
 @functools.total_ordering
 class KeyedComparisonMixin(KeyedEqualityMixin):
-   def __lt__(self, other):
-       return self.__key__() < other.__key__()
+    def __lt__(self, other):
+        return self.__key__() < other.__key__()
+
 
 class KeyedHashingMixin(KeyedEqualityMixin):
     def __hash__(self):
         return hash(self.__key__())
-
--- test_mixins.py
+++ test_mixins.py
@@ -6,9 +6,11 @@ class Simple(object):
     def __init__(self, x):
         self.x = x
 
+
 class EqualityClass(Simple, KeyedEqualityMixin):
     def __key__(self):
         return self.x
+
 
 class TestDefault(TC):
     def test(self):
@@ -16,6 +18,7 @@ class TestDefault(TC):
         self.assertFalse(x == y)
         self.assertTrue(x < y or y < x)
         self.assertFalse(hash(x) == hash(y))
+
 
 class TestEquality(TC):
     def test(self):
@@ -25,7 +28,10 @@ class TestEquality(TC):
         self.assertFalse(x == z)
         self.assertTrue(x != z)
 
-class ComparisonClass(EqualityClass, KeyedComparisonMixin): pass
+
+class ComparisonClass(EqualityClass, KeyedComparisonMixin):
+    pass
+
 
 class TestComparision(TC):
     def test(self):
@@ -38,11 +44,23 @@ class TestComparision(TC):
         self.assertTrue(x <= z)
         self.assertFalse(x > z)
         self.assertFalse(x >= z)
+        self.assertFalse(z < x)
+        self.assertFalse(z <= x)
+        self.assertTrue(z > x)
+        self.assertTrue(z >= x)
 
-class HashClass(EqualityClass, KeyedHashingMixin): pass
 
-class TestComparision(TC):
+class HashClass(EqualityClass, KeyedHashingMixin):
+    pass
+
+
+class TestHash(TC):
     def test(self):
         x, y, z = HashClass(1), HashClass(1), HashClass(2)
         self.assertTrue(hash(x) == hash(y))
         self.assertFalse(hash(x) != hash(y))
+
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
