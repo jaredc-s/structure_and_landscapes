8e1ef391ba3ef98dd72ce87a94426850e86f57da
Joseph Marcus <josephhmarcus@gmail.com>
2012-06-19 17:24:45 -0700

    added docstrings to bitstring.py

--- bitstring.py
+++ bitstring.py
@@ -7,24 +7,60 @@ in python tuples. Bitstring instances are immutable.
 class Bitstring(object):
 
     def __init__(self, iterable):
+        """
+        init takes a single argument
+
+        If that argument is a string, where the string has a character of 1,
+        the bit string will be true at that position.
+
+        If the argument is not a string,
+        the iterable will be converted to a tuple
+        composed of booleans where each value is coerced to a boolean.
+
+        for example:
+             "1001" --> (True, False, False, True)
+            [1, 0, None, [], 'hello'] --> (True, False, False, False, True)
+        """
+
         if isinstance(iterable, str):
             self._value = tuple(char == "1" for char in iterable)
         else:
-            self._value = tuple(bool(char) for char in iterable)
+            self._value = tuple(bool(value) for value in iterable)
 
     def __len__(self):
+        """
+        length of a bitstring is the number of elements
+        """
         return len(self._value)
 
     def __getitem__(self, key):
+        """
+        bitstrings can be accessed by index
+        for example:
+            Bitstring("101")[0] = True
+            Bitstring("101")[1] = False
+        """
         return self._value[key]
 
     def __iter__(self):
+        """
+        Bitstrings are iterable
+        """
         return iter(self._value)
 
     def __hash__(self):
+        """
+        Bitstrings are hashable
+        Bitstrings can be used as keys for dictionaries or in sets
+        """
         return hash(self._value)
 
     def __eq__(self, other):
+        """
+        Bitstrings compare equal if they are the
+        same class and identical by elements
+        """
+
         if not isinstance(other, type(self)):
             return False
         return self._value == other._value
@@ -33,7 +69,13 @@ class Bitstring(object):
         return not self == other
 
     def __repr__(self):
+        """
+        Bitstrings can be printed
+        """
         def bool_to_str(boolean):
+            """
+            Helper Function converting a bool to the characters 1 or 0
+            """
             if boolean:
                 return "1"
             return "0"
@@ -42,11 +84,21 @@ class Bitstring(object):
         return "{}({!r})".format(self.__class__.__name__, value_as_string)
 
     def hamming_distance(self, other):
+        """
+        This returns the number of differences between two bitstrings
+        note bitstrings must be the same length
+        if different lengths only compare to the length of the shorter
+        """
         return len(tuple(None for (self_pos, other_pos) in zip(
             self, other) if self_pos != other_pos))
 
 
 def flip_positions(bitstring_instance, positions_to_flip):
+    """
+    Function takes a bitstring and a list of indicies to
+    flip (True --> False), (False-->True)
+    returns a new bitstring with the modification
+    """
     values = list(bitstring_instance)
     for pos in positions_to_flip:
         values[pos] = not values[pos]
