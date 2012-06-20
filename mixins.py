4a87184913c28b4b3aa51963d5e8c15747c7267e
Joshua Nahum <josh@nahum.us>
2012-06-20 13:56:03 -0700

    added guards for different classes in mixins

--- mixins.py
+++ mixins.py
@@ -3,15 +3,19 @@ import functools
 
 class KeyedEqualityMixin(object):
     def __eq__(self, other):
+        if type(self) != type(other):
+            return False
         return self.__key__() == other.__key__()
 
     def __ne__(self, other):
-        return self.__key__() != other.__key__()
+        return not self == other
 
 
 @functools.total_ordering
 class KeyedComparisonMixin(KeyedEqualityMixin):
     def __lt__(self, other):
+        if type(self) != type(other):
+            return id(self) < id(other)
         return self.__key__() < other.__key__()
 
 
