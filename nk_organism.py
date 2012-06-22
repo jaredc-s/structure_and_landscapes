6b276af48b756f9458849eb9055edcc1bd54bf61
Jared Carlson-Stevermer <jmcs@utexas.edu>
2012-06-22 14:29:20 -0700

    Finished off nk_organism instantiation and added tests

--- nk_organism.py
+++ nk_organism.py
@@ -1,9 +1,24 @@
-import nk_model
 import mixins
+from bitstring_organism import Organism as BOrg
 import bitstring_organism
+from mutate import mutate_value
 
-class Organism(bitstring_organism):
-     def __init__(self, value, look_up_table):
-         self.look_up_table = look_up_table
-         super(Organism, self).__init__(self, value)
+class Organism(BOrg):
+     def __init__(self, value, nk_model):
+         self.nk_model = nk_model
+         super(Organism, self).__init__(value)
+     
+     @property
+     def fitness(self):
+          return self.nk_model.determine_fitness(self.value)
+
+     def mutate(self):
+         """
+         the mutate method of an organism calls the module
+         mutate and returns a new organism with the mutation
+         note: original organism is unchanged
+         """
+         print self.value
+         mutated_value = mutate_value(self.value)
+         return Organism(mutated_value, self.nk_model)
 
--- test_nk_organism.py
+++ test_nk_organism.py
@@ -1,17 +1,32 @@
 import unittest
 from unittest import TestCase as TC
-
+import nk_model
+import bitstring
 import nk_organism
 from nk_organism import Organism
 
+
 class TestOrganism(TC):
+    def setUp(self):
+        self.value = bitstring.Bitstring("100")
+        self.lookup = [[1, 0.5], [0.2, 0.4], [0.1, 0.8]]
+        self.model = nk_model.NKModel(3, 0, self.lookup)
+        self.org = Organism(self.value, self.model) 
+   
     def test_init(self):
-        Organism('1110', MockLookUpTable.create_table())
+        self.assertEqual(self.org.value,self.value)
+        self.assertEqual(self.org.nk_model.contribution_lookup_table,
+                         self.model.contribution_lookup_table)
 
-class MockLookUpTable(object):
-    def __init__(self, n=3, k=1):
-        self.n = n
-        self.k = k
+    def test_fitness(self):
+        expected_fit = (0.5 + 0.2 + 0.1)/float(3)
+        self.assertEqual(expected_fit, self.org.fitness)
 
-    def create_table(self):
-        return [[i for i in range(2 ** (k + 1))] for _ in range(n)]
+    def test_equality(self):
+        other = Organism(bitstring.Bitstring("001"), self.model)
+        self.assertNotEqual(self.org, other)
+        self.assertEqual(self.org, self.org)
+
+    def test_mutate(self):
+        other = self.org.mutate()
+        self.assertNotEqual(self.org, other)
