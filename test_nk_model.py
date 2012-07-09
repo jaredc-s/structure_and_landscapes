8b8eaaa2a27733420ff84f9e37914d93145f27e1
Joshua Nahum <josh@nahum.us>
2012-07-09 11:44:49 -0700

    merged nk_model and nk_model_with_genes

--- test_nk_model.py
+++ test_nk_model.py
@@ -1,4 +1,5 @@
 from unittest import TestCase as TC
+import nk_model
 from nk_model import *
 from random import Random
 from bitstring import Bitstring
@@ -42,7 +43,7 @@ class TestNKModel(TC):
         self.assertEqual(rug_fit, expected_rugged_fit)
 
 
-class TestModule(TC):
+class TestModuleWitoutGenes(TC):
     def test_generate_constribution_lookup_table(self):
         n, k = 10, 2
         clt = generate_contribution_lookup_table(n, k)
@@ -58,7 +59,7 @@ class TestModule(TC):
         self.assertEqual(Bitstring("1001"), sub2)
         sub3 = get_substring_with_wrapping(b, k=3, i=3)
         self.assertEqual(Bitstring("0011"), sub3)
-        
+
     def test_deconstruct_bitstring(self):
         B = Bitstring
         b = B("11001")
@@ -67,7 +68,7 @@ class TestModule(TC):
                     B("0011"), B("0111"),
                     B("1110")]
         self.assertEqual(subs, expected)
-        
+
     def test_deconstruct_random(self):
         B = Bitstring
         b = Bitstring("100")
@@ -82,3 +83,62 @@ class TestModule(TC):
         self.assertEqual(len(l[0]), 3)
         self.assertEqual(l[0][0], 0)
 
+class TestNKWithGenes(TC):
+    def setUp(self):
+        self.list_of_strings = [Bitstring("010"), Bitstring("100"),
+                           Bitstring("001")]
+
+    def test_determine_fitness(self):
+        contrib_table = [[0.25, 0.8, 0.15], [0.9, 0.4, 0.55], [0.05, 0.7, 0.3]]
+        nk = NKWithGenes(1, 0, 2, 2)
+        bit_org = Bitstring("0101")
+        self.assertNotEqual(0, nk.determine_fitness(bit_org))
+
+    def test_passed_contribution_table(self):
+        contrib_table = generate_contribution_lookup_table(3, 2)
+        nk = NKWithGenes(1, 1, 2, 2, contrib_table)
+        self.assertEqual(contrib_table, nk.contribution_lookup_table)
+
+    def test_gene_divider(self):
+        gene = Bitstring("101010101010")
+        nk = NKWithGenes(2, 2, 3, 4)
+        divided = nk.divide_to_genes(gene)
+        self.assertEqual(len(divided), 4)
+        self.assertEqual(len(divided[0]), 3)
+        self.assertEqual(Bitstring("010"), divided[-1])
+
+class TestModuleWithGenes(TC):
+    def setUp(self):
+        self.depend = generate_dependencies(2, 2, 3, 3)
+        self.list_of_strings = [Bitstring("010"), Bitstring("100"),
+                           Bitstring("001")]
+
+    def test_dependencies(self):
+        long_depend = generate_dependencies(2, 4, 3, 3)
+        self.assertEqual(len(long_depend), 3)
+        self.assertEqual(len(long_depend[0]), 3)
+        self.assertEqual(len(long_depend[-1]), 3)
+
+    def test_passed_depend(self):
+        dependencies = [[[(2, 0), (1, 2)], [(1, 1), (2, 1)],[(1, 0), (2, 2)]],
+                        [[(1, 2), (1, 1)], [(0, 0), (1, 1)], [(0, 1), (0, 1)]],
+                        [[(2, 2), (2, 0)], [(2, 2), (2, 0)], [(1, 1), (0, 1)]]]
+        subs = generate_sub_bitstring(self.list_of_strings,
+                                                          dependencies, 2)
+        self.assertEqual(subs[0][1], Bitstring("10000"))
+
+    def test_sub_bitstring(self):
+
+        full = generate_sub_bitstring(self.list_of_strings,
+                                                           self.depend, 2)
+        self.assertEqual(len(full), len(self.list_of_strings))
+        self.assertEqual(len(full[0]), 3)
+        self.assertEqual(len(full[0][0]), 5)
+
+    def test_linear_jump(self):
+        linear_model = generate_linear_bistring(
+            self.list_of_strings, 2, 1)
+        self.assertEqual(4, len(linear_model[0][0]))
+        self.assertEqual(3, len(linear_model))
+        self.assertEqual(3, len(linear_model[0]))
+        self.assertEqual(Bitstring("0101"), linear_model[0][0])
