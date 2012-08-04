88885699e94e5f9bd639559d50164007adb15182
Joshua Nahum <josh@nahum.us>
2012-08-04 16:44:32 -0700

    added scipy/matplotlib to install

--- amazon.sh
+++ amazon.sh
@@ -2,7 +2,7 @@ apt-get -y update
 apt-get -y upgrade
 #May be unneeded
 #apt-get -y dist-upgrade
-apt-get -y install make git python-numpy build-essential python2.7-dev python-pip vim
+apt-get -y install make git python-scipy python-matplotlib python-numpy build-essential python2.7-dev python-pip vim
 wget http://www.tbi.univie.ac.at/~ronny/RNA/ViennaRNA-2.0.7.tar.gz
 tar -xf ViennaRNA-2.0.7.tar.gz
 cd ViennaRNA-2.0.7
