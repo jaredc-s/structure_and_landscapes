ea2385fd2309c3239083c32fdb94c7f82c44c758
Joshua Nahum <josh@nahum.us>
2012-08-04 14:57:36 -0700

    small changes

--- amazon.sh
+++ amazon.sh
@@ -1,4 +1,13 @@
 apt-get -y update
 apt-get -y dist-upgrade
-apt-get -y install make git python python-numpy build-essential python-dev python-pip
+apt-get -y install make git python-numpy build-essential python-dev python-pip
+wget http://www.tbi.univie.ac.at/~ronny/RNA/ViennaRNA-2.0.7.tar.gz
+tar -xf ViennaRNA-2.0.7.tar.gz
+cd ViennaRNA-2.0.7
+./configure
+make
+make install
+cd ~
 git clone git://github.com/nahumj/structure_and_landscapes.git
+cd structure_and_landscapes
+make install
