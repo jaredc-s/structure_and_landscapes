d4f6e27e72c8bce14b178ab253a6f357a29030f7
Joshua Nahum <josh@nahum.us>
2012-08-04 19:59:43 -0700

    renamed set_script

--- setup_script.sh
+++ setup_script.sh
@@ -2,7 +2,7 @@ apt-get -y update
 #May be unneeded
 #apt-get -y upgrade
 #apt-get -y dist-upgrade
-apt-get -y install make git python-numpy python-scipy python-matplotlib build-essential python2.7-dev python-pip vim screen
+apt-get -y install make git python-numpy python-scipy python-matplotlib build-essential python2.7-dev python-pip vim screen python-profiler python-wxgtk2.8 python-setuptools
 wget http://www.tbi.univie.ac.at/~ronny/RNA/ViennaRNA-2.0.7.tar.gz
 tar -xf ViennaRNA-2.0.7.tar.gz
 cd ViennaRNA-2.0.7
@@ -10,3 +10,5 @@ cd ViennaRNA-2.0.7
 make
 make install
 export PYTHONPATH=$PYTHONPATH:~
+cd ~/structure_and_landscapes
+make install
