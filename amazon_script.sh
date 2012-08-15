9fc234f02c34edbf72b1c6fd8e600de34d1eeef3
Joshua Nahum <josh@nahum.us>
2012-08-15 12:44:29 -0700

    removed accidental add

--- amazon_script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd filtered_fastas
-for file in *.gz; do gunzip $file; done;
-for file in *; do breseq -r ../w3110_ref.gb $file -o ../breseq_output/${file%.*}; done;
-cd ../breseq_output
-for folder in  *; do cp "${folder}"/output/output.gd ~/Dropbox/mutations/"${folder}".gd; done;
