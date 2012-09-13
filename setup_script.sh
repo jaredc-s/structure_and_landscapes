1d261db10628a53422c34d23d15118167df8b2b7
Joshua Nahum <josh@nahum.us>
2012-09-13 13:54:09 -0700

    added cython and nose to install script

--- setup_script.sh
+++ setup_script.sh
@@ -2,7 +2,7 @@ apt-get -y update
 #May be unneeded
 #apt-get -y upgrade
 #apt-get -y dist-upgrade
-apt-get -y install make git python-numpy python-scipy python-matplotlib build-essential python2.7-dev python-pip vim screen python-profiler python-wxgtk2.8 python-setuptools
+apt-get -y install make git python-numpy python-scipy python-matplotlib build-essential python2.7-dev python-pip vim screen python-profiler python-wxgtk2.8 python-setuptools cython python-nose
 wget http://www.tbi.univie.ac.at/~ronny/RNA/ViennaRNA-2.0.7.tar.gz
 tar -xf ViennaRNA-2.0.7.tar.gz
 cd ViennaRNA-2.0.7
