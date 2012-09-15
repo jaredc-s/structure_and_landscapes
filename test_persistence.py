89e426ee1dc6b447032aa8b9c824d9b2c54a8f18
Joshua Nahum <josh@nahum.us>
2012-09-14 19:38:33 -0700

    test for persistence

--- test_persistence.py
+++ test_persistence.py
@@ -1,10 +1,18 @@
 from unittest import TestCase as TC
 import persistence
+import tempfile
+import os.path
+import shutil
+
 class TestModule(TC):
+
     def test_save_load(self):
+        temp_dir = tempfile.mkdtemp()
         filename = "test.shelf"
+        filepath = os.path.join(temp_dir, filename)
         key = "test_key"
         value = [42]
-        persistence.save(filename, key, value)
-        result = persistence.load(filename, key)
+        persistence.save(filepath, key, value)
+        result = persistence.load(filepath, key)
         self.assertEqual(value, result)
+        shutil.rmtree(temp_dir)
