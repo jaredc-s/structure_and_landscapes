38dd4bab11a13ccf35b9c51b7796c4f258cb565a
Joshua Nahum <josh@nahum.us>
2012-09-14 20:16:56 -0700

    using closing context manager

--- persistence/persistence.py
+++ persistence/persistence.py
@@ -11,17 +11,15 @@ Final population
 import shelve
 import pickle
 import uuid
+from contextlib import closing
 
 def save(filepath, key, value):
-    shelf = shelve.open(filepath, flag='c', protocol=pickle.HIGHEST_PROTOCOL)
-    shelf[key] = value
-    shelf.close()
+    with closing(get_shelf(filepath)) as shelf:
+        shelf[key] = value
 
 def load(filepath, key):
-    shelf = shelve.open(filepath, flag='r', protocol=pickle.HIGHEST_PROTOCOL)
-    result = shelf[key]
-    shelf.close()
-    return result
+    with closing(get_shelf(filepath)) as shelf:
+        return shelf[key]
 
 def get_shelf(filepath):
     """
