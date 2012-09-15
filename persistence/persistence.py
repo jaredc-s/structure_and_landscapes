9a1e88bcd707664b13b9dcdfe0f887ec1600bbc5
Joshua Nahum <josh@nahum.us>
2012-09-14 20:13:13 -0700

    added more persistence functions

--- persistence/persistence.py
+++ persistence/persistence.py
@@ -10,6 +10,7 @@ Final population
 """
 import shelve
 import pickle
+import uuid
 
 def save(filepath, key, value):
     shelf = shelve.open(filepath, flag='c', protocol=pickle.HIGHEST_PROTOCOL)
@@ -21,3 +22,16 @@ def load(filepath, key):
     result = shelf[key]
     shelf.close()
     return result
+
+def get_shelf(filepath):
+    """
+    Returns the shelf object. Don't forget to close.
+    """
+    return shelve.open(filepath, flag='c', protocol=pickle.HIGHEST_PROTOCOL)
+
+def save_with_unique_key(filepath, value):
+    """
+    Saves a given object with a unique key.
+    """
+    key = str(uuid.uuid4())
+    save(filepath, key, value)
--- persistence/test_persistence.py
+++ persistence/test_persistence.py
@@ -3,16 +3,50 @@ import persistence
 import tempfile
 import os.path
 import shutil
+from contextlib import closing
 
+import structure_and_landscapes.integer.integer_organism as org
 class TestModule(TC):
-
-    def test_save_load(self):
+    def setUp(self):
+        """
+        Generates a temp file path.
+        """
         temp_dir = tempfile.mkdtemp()
         filename = "test.shelf"
         filepath = os.path.join(temp_dir, filename)
-        key = "test_key"
-        value = [42]
-        persistence.save(filepath, key, value)
-        result = persistence.load(filepath, key)
-        self.assertEqual(value, result)
+        self.temp_file = filepath
+
+    def tearDown(self):
+        """
+        Closes temp file.
+        """
+        temp_dir = os.path.dirname(self.temp_file)
         shutil.rmtree(temp_dir)
+
+
+    def test_save_load(self):
+        key = "test_key"
+        value = 42
+        persistence.save(self.temp_file, key, value)
+        result = persistence.load(self.temp_file, key)
+        self.assertEqual(value, result)
+
+    def test_save_load_org(self):
+        key = "test_key"
+        value = org.Organism(3)
+        persistence.save(self.temp_file, key, value)
+        result = persistence.load(self.temp_file, key)
+        self.assertEqual(value, result)
+
+    def test_get_shelf(self):
+        persistence.save(self.temp_file, "a", 1)
+        persistence.save(self.temp_file, "b", 2)
+        persistence.save(self.temp_file, "c", 3)
+        with closing(persistence.get_shelf(self.temp_file)) as shelf:
+            self.assertEqual(len(shelf), 3)
+
+    def test_save_with_unique_key(self):
+        for _ in range(10):
+            persistence.save_with_unique_key(self.temp_file, None)
+        with closing(persistence.get_shelf(self.temp_file)) as shelf:
+            self.assertEqual(len(shelf), 10)
