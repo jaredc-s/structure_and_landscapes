f1b30140f6e38a25871c7d79c4612cd80a64a90a
Joshua Nahum <josh@nahum.us>
2012-09-26 09:39:20 -0700

    Start of run module

---
+++ persistence/run.py
@@ -0,0 +1,16 @@
+"""
+This module contains a class (Run) that encapsulate the
+parameters and results of a single evolutionary simulation.
+"""
+import persistence
+
+class Run(object):
+
+    def __init__(self, initial_population, parameters, shelf_filepath):
+        self.initial_population = initial_population
+        self.parameters = parameters
+        self.shelf_filepath = shelf_filepath
+
+    def run(self):
+        self.results = None
+        persistence.save_with_unique_key(self, self.shelf_filepath)
