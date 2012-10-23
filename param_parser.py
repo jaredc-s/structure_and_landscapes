a4152f647223bdc224f6ab9f6cc2390bbc3123a6
Joshua Nahum <josh@nahum.us>
2012-10-23 09:17:22 -0700

    Rewrote param_parser to not be dependant on lineno

--- param_parser.py
+++ param_parser.py
@@ -15,9 +15,9 @@ import scipy.stats as st
 import matplotlib.pyplot as plt
 
 
-def run_struc(struc_pop):
+def run_struc(struc_pop, number_of_generations):
     fit_list = []
-    for gen in range(updates):
+    for gen in range(number_of_generations):
         one_gen = [org.fitness for pop in struc_pop for org in pop]
         fit_list.append(one_gen)
         pop.advance_generation()
@@ -34,60 +34,66 @@ def print_best_structure(pop):
     #print vienna_distance.fold(rna_organism.OPTIMAL_RNA_SEQUENCE)+'*'
     #print vienna_distance.fold(best_org.value), best_org.fitness
 
-
-
-
-fp = open("parameters.cfg")
-fp.readline()
-fp.readline()
-fp.readline()
-fp.readline()
-mut_rate = float(fp.readline().split(":")[1].strip())
-num_org = int(fp.readline().split(":")[1].strip())
-pops = int(fp.readline().split(":")[1].strip())
-mig_rate = float(fp.readline().split(":")[1].strip())
-swap_rate = float(fp.readline().split(":")[1].strip())
-org_type = fp.readline().split(":")[1].strip()
-len_org = int(fp.readline().split(":")[1].strip())
-len_gene = int(fp.readline().split(":")[1].strip())
-num_gene = int(fp.readline().split(":")[1].strip())
-k_tot = int(fp.readline().split(":")[1].strip())
-k_intra = int(fp.readline().split(":")[1].strip())
-updates = int(fp.readline().split(":")[1].strip())
+class OrgException(Exception):
+    pass
 
 random.seed(1)
 
-if org_type == "RNA":
-    if pops <= 1:
-        swap_rate = 0
-        mig_rate = 0
+parameter_settings = {}
+with open("parameters.cfg") as parameters_file:
+    for line in parameters_file:
+        line_without_comments, _, _ = line.partition("#")
+        line_stripped = line_without_comments.strip()
+        if not line_stripped:
+            continue
+        parameter, _, value = line_stripped.partition(":")
+        parameter_settings[parameter] = value
+
+print(parameter_settings)
+
+if parameter_settings["Organism Type"] == "RNA":
     org = rna_organism.random_organism()
-    org_list = [org for _ in range(num_org)]
-    pop_list = [Population(org_list) for _ in range(pops)]
-    structured_pop = Structured_Population(pop_list,
-                                           migration_rate=mig_rate,
-                                           proportion_of_pop_swapped=swap_rate)
-    run_struc(structured_pop)
-
-elif org_type == "Bitstring":
+elif parameter_settings["Organism Type"] == "Bitstring":
     pass
-elif org_type == "NK Model":
-    if pops <= 1:
-        mig_rate = 0
-        swap_rate = 0
-    b = bs.bitstring.random_string(len_org)
+elif parameter_settings["Organism Type"] == "NK Model":
+    b = bs.bitstring.random_string(int(parameter_settings["Length of Org"]))
     nk_fac = bs.nk_model.NKModelFactory()
-    nk_org = bs.nk_organism.Organism(
-        b, nk_fac.consecutive_dependencies_multigene(
-                n_per_gene=len_gene, number_of_genes=num_gene, k_intra_gene=k_intra, k_total=k_tot))
-    org_list = [nk_org for _ in range(num_org)]
-    pop_list = [Population(org_list, mut_rate) for _ in range(pops)]
-    structured_pop = Structured_Population(pop_list, 
-                                           migration_rate=mig_rate,
-                                           proportion_of_pop_swapped=swap_rate)
-    run_struc(structured_pop)
+    length_of_gene = int(parameter_settings["Length of Gene"])
+    number_of_genes = int(parameter_settings["Number of Genes"])
+    k_intra = int(parameter_settings["K-intra"])
+    k_total = int(parameter_settings["K-total"])
 
-
+    nk_model =  nk_fac.consecutive_dependencies_multigene(
+            n_per_gene=length_of_gene,
+            number_of_genes=number_of_genes,
+            k_intra_gene=k_intra,
+            k_total=k_total)
+    org = bs.nk_organism.Organism(
+            b, nk_model)
 else:
-    raise TypeError("Not a valid org type")
+    raise OrgException("Not a valid org type")
+
+
+
+number_of_pops = int(parameter_settings["Number of Populations"])
+if number_of_pops <= 1:
+    mig_rate = 0.0
+    swap_rate = 0.0
+else:
+    mig_rate = float(parameter_settings["Migration Rate"])
+    swap_rate = float(parameter_settings["Proportion of Population Migrated"])
+
+orgs_per_population = int(parameter_settings["Orgs per Population"])
+
+
+org_list = [org for _ in range(orgs_per_population)]
+pop_list = [Population(org_list) for _ in range(number_of_pops)]
+structured_pop = Structured_Population(
+        pop_list,
+        migration_rate=mig_rate,
+        proportion_of_pop_swapped=swap_rate)
+run_struc(structured_pop, int(parameter_settings["Number of Generations"]))
+
+
+
 
