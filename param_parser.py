ecafb5a394501f276094cb433cb752d127eed354
Joshua Nahum <josh@nahum.us>
2012-10-24 10:32:26 -0700

    Args parse

--- param_parser.py
+++ param_parser.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python2
 import random
+import argparse
 from persistence.run import process_and_run
+
+
+parser = argparse.ArgumentParser(
+        description="Command Line Interface for the structure and landscapes package.")
+parser.add_argument('parameters_file', help="specify the location of the configuration file")
+args = parser.parse_args()
+print(args)
 
 random.seed(1)
 
+
 parameter_settings = {}
-with open("parameters.cfg") as parameters_file:
+with open(args.parameters_file, "r") as parameters_file:
     for line in parameters_file:
         line_without_comments, _, _ = line.partition("#")
         line_stripped = line_without_comments.strip()
