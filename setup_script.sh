93ab1bbc633271f43360b1a79334d2e954b6be10
Joshua Nahum <josh@nahum.us>
2012-11-06 15:50:37 -0800

    Simplified Install Path

--- setup_script.sh
+++ setup_script.sh
@@ -1,17 +1,17 @@
+#!/usr/bin/env bash
 apt-get -y update
 #May be unneeded
 #apt-get -y upgrade
 #apt-get -y dist-upgrade
 apt-get -y install make python-numpy python-scipy python-matplotlib build-essential python2.7-dev python-pip vim screen python-profiler python-wxgtk2.8 python-setuptools git-core cython python-nose
-cd
+cd ..
 wget http://www.tbi.univie.ac.at/~ronny/RNA/ViennaRNA-2.0.7.tar.gz
 tar -xf ViennaRNA-2.0.7.tar.gz
 cd ViennaRNA-2.0.7
 ./configure
 make
 make install
-export PYTHONPATH=$PYTHONPATH:~
-cd
-git clone https://github.com/nahumj/structure_and_landscapes.git
+cd ..
+export PYTHONPATH=$PYTHONPATH:$(pwd)
 cd structure_and_landscapes
 make install
