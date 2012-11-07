d572bfc8bc2cd88e8f3fd96270f768b415139f44
Joseph Marcus <josephhmarcus@gmail.com>
2012-11-07 12:24:04 -0800

    converted lists to iterables!

--- analysis/extract_data.py
+++ analysis/extract_data.py
@@ -2,58 +2,52 @@ from structure_and_landscapes import persistence
 from structure_and_landscapes.persistence import run
 from structure_and_landscapes.persistence import persistence
 from scipy import stats
+import itertools
 
 """
 write tests...
+go through all runs just once and pull out data!
 """
 
-shelf_path = '/vagrant/saved_runs.dat'
-run_list = list(persistence.values(shelf_path))
-
-def filter_runs(run_list, parameters_dictionary):
+def filter_runs(runs, parameters_dictionary):
     """
-    takes a list of runs and filters them by specified parameters
-    passed in. Retruns a list of filtered runs.
     """
-
-    for run in run_list:
+    def same_parameters(run):
         same_values = [run.parameters.get(key) == value
                 for key, value in parameters_dictionary.items()]
-        if all(same_values):
-            yield run
+        return all(same_values)
 
+    return itertools.ifilter(same_parameters, runs)
 
-def organism_fitness_for_each_run(run_list):
+def organism_fitness_for_each_run(runs):
     """
-    returns a list of lists where the nested list holds all organisms final fitness. The
-    outer list carries each run.
     """
-    org_fitness_list = []
-    sub_population_fitness_list = []
-    for run in run_list:
+    def sub_population_fitness(sub_population):
+        for org in sub_population:
+            yield org.fitness
+
+    for run in runs:
         for sub_population in run.final_population:
-            for org in sub_population:
-                org_fitness_list.append(org.fitness)
-            sub_population_fitness_list.append(org_fitness_list)
-            org_fitness_list = []
+            yield sub_population_fitness(sub_population)
 
-    return sub_population_fitness_list
-
-def mean_fitness_for_each_run(run_list):
+def mean_fitness_for_each_run(runs):
     """
-    returns a list of mean final fitnesses for each run
     """
-    return [run.final_population.mean_fitness() for run in run_list]
+    return [run.final_population.mean_fitness() for run in runs]
 
 def max_fitness_for_each_run(run_list):
     """
-    returns a list of the max final fitness for each run
     """
     return [run.final_population.max_fitness() for run in run_list]
 
 
-filtered_run_list = list(filter_runs(run_list, {'Organism Type':'NK Model'}))
-print mean_fitness_for_each_run(filtered_run_list)
-print max_fitness_for_each_run(filtered_run_list)
-#print organism_fitness_for_each_run(filtered_run_list)
+shelf_path = '/vagrant/saved_runs.dat'
+runs = persistence.values(shelf_path)
+
+filtered_runs = filter_runs(runs, {'Organism Type':'NK Model'})
+#runs1, runs2 = itertools.tee(filtered_runs, 2)
+#print mean_fitness_for_each_run(runs1)
+#print max_fitness_for_each_run(runs2)
+#population_fitness = list(organism_fitness_for_each_run(filtered_runs))
+#print map(list, population_fitness)
 
