fe75410a85e4f2f9791661918c7784990e11a0f6
Joseph Marcus <josephhmarcus@gmail.com>
2012-11-07 12:24:46 -0800

    added rough histogram plot

--- analysis/basic_plot.py
+++ analysis/basic_plot.py
@@ -1,6 +1,7 @@
 from numpy import *
 from pylab import *
 from matplotlib import *
+import matplotlib.pyplot as plt
 from scipy import *
 
 
@@ -11,3 +12,12 @@ def plot_average_fitness(fitness):
     ylabel("Average Fitness")
     plot(values)
     show()
+
+def plot_fitness_distribution(fitness_list):
+    mu, sigma = 100, 15
+    x = mu + sigma * numpy.random.randn(100000)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    n, bins, patches = ax.hist(x, 50, normed=1, facecolor='green', alpha=.75)
+
