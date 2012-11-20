022801c92193b6d753ccf14d30e764baf7023280
Joshua Nahum <josh@nahum.us>
2012-11-20 15:08:09 -0800

    moved functionailty out of root dir

--- run_config.py
+++ run_config.py
@@ -2,83 +2,9 @@
 import random
 import argparse
 from structure_and_landscapes.run_management.run import process_and_run
+from structure_and_landscapes.run_management.parameters import get_parameter_settings
 import datetime
-import itertools
 
-
-def get_parameter_settings(parameters_file_contents):
-    """
-    Returns a list of parameter settings dictionaries corresponding
-    to the configuration settings specified (comma seperated values
-    are performed in all possible combinations).
-    """
-
-    settings = parse_contents_to_settings_dict(parameters_file_contents)
-    general_settings, multiple_settings = split_lists_out(settings)
-
-    list_of_settings = []
-    for specific_values in dictionary_product(multiple_settings):
-        specific_values.update(general_settings)
-        list_of_settings.append(specific_values)
-    return list_of_settings
-
-
-def split_lists_out(dictionary):
-    """
-    Splits given dictionary into two, first the mappings were the value
-    is not a list, and the second the mappings whose values are lists.
-    """
-    without_lists = {}
-    with_lists = {}
-    for key, value in dictionary.items():
-        if isinstance(value, list):
-            with_lists[key] = value
-        else:
-            without_lists[key] = value
-    return without_lists, with_lists
-
-
-def parse_contents_to_settings_dict(parameters_file_contents):
-    """
-    Parse the contents of the parameters file.
-    Comments are after '#' symbols. 'xxx:yyy' are the key-value mappings.
-    If multiple settings are included ('xxx:y,z'),
-    then the list of values is mapped.
-    """
-    settings = {}
-    for line in parameters_file_contents.splitlines():
-        line_without_comments, _, _ = line.partition("#")
-        line_stripped = line_without_comments.strip()
-        if not line_stripped:
-            continue
-        raw_parameter, _, raw_value = line_stripped.partition(":")
-        parameter = raw_parameter.strip()
-        value = raw_value.strip()
-        values_split = value.split(",")
-        if len(values_split) == 1:
-            settings[parameter] = value.strip()
-        else:
-            settings[parameter] = [value_split.strip()
-                                   for value_split in values_split]
-    return settings
-
-
-def dictionary_product(key_to_list_of_values):
-    """
-    Takes a dicitonary where each key (string) is a parameter that points
-    points to a list of specifications.
-    Returns a flattened list of dictionaries with
-    full factorial possibilities of the passed in dictionary.
-    """
-    keys = key_to_list_of_values.keys()
-    values = key_to_list_of_values.values()
-    values_product = itertools.product(*values)
-    result = []
-    for values_tuple in values_product:
-        list_of_pairs = zip(keys, values_tuple)
-        specific_dict = dict(list_of_pairs)
-        result.append(specific_dict)
-    return result
 
 
 def parse_arguments():
--- run_management/test_parameters.py
+++ run_management/test_parameters.py
@@ -1,5 +1,5 @@
 from unittest import TestCase as TC
-import run_config
+import parameters
 
 class TestModule(TC):
 
@@ -9,7 +9,7 @@ class TestModule(TC):
         expected_results_0 = {'A':1, 'B':'a', 'C':True}
         expected_results_1 = {'A':1, 'B':'a', 'C':False}
         expected_results_11 = {'A':3,'B':'b', 'C':False}
-        results = run_config.dictionary_product(settings)
+        results = parameters.dictionary_product(settings)
         self.assertEqual(len(results), 12)
         self.assertIn(expected_results_0, results)
         self.assertIn(expected_results_1, results)
@@ -19,19 +19,19 @@ class TestModule(TC):
         settings = {'A':"4",
                 'B':"True",
                 'C':["1", "2"]}
-        without_lists, with_lists = run_config.split_lists_out(settings)
+        without_lists, with_lists = parameters.split_lists_out(settings)
         self.assertIn('A', without_lists)
         self.assertIn('B', without_lists)
         self.assertIn('C', with_lists)
 
     def test_parse_contents_to_settings_dict_single(self):
         contents = "A:1\nB:2\nC:3"
-        settings = run_config.parse_contents_to_settings_dict(contents)
+        settings = parameters.parse_contents_to_settings_dict(contents)
         self.assertEqual({"A":"1", "B":"2", "C":"3"}, settings)
 
     def test_parse_contents_to_settings_dict_multiple(self):
         contents = "A:1,2\nB:3\nC:4,5"
-        settings = run_config.parse_contents_to_settings_dict(contents)
+        settings = parameters.parse_contents_to_settings_dict(contents)
         self.assertEqual({"A":["1", "2"], "B":"3", "C":["4", "5"]}, settings)
 
     def test_comments_whitespace_parseing(self):
@@ -41,7 +41,7 @@ class TestModule(TC):
         Multiple words : 4, 5, 6 # End comment
         # After comments
         """
-        settings = run_config.parse_contents_to_settings_dict(contents)
+        settings = parameters.parse_contents_to_settings_dict(contents)
         self.assertEqual({"Multiple words":["4", "5", "6"]}, settings)
 
     def test_get_parameters_settings(self):
@@ -53,7 +53,7 @@ class TestModule(TC):
         expected_start = {'A':'1', 'B':'2', 'C':'4'}
         expected_end = {'A':'1', 'B':'3', 'C':'5'}
 
-        result = run_config.get_parameter_settings(contents)
+        result = parameters.get_parameter_settings(contents)
         self.assertEqual(len(result), 4)
         self.assertIn(expected_start, result)
         self.assertIn(expected_end, result)
