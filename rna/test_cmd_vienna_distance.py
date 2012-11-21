39d3c847d8eeb0062c21f87f30ced1c33cd78b64
Joshua Nahum <josh@nahum.us>
2012-11-20 16:17:24 -0800

    removed redundant code

--- rna/test_cmd_vienna_distance.py
+++ rna/test_cmd_vienna_distance.py
@@ -1,29 +1,7 @@
 from unittest import TestCase as TC
-import cmd_vienna_distance as vienna_distance
+import cmd_vienna_distance
+import test_vienna_distance
 
 
-class TestModule(TC):
-
-    def test_distance_unequal(self):
-        distance = vienna_distance.get_distance(
-            "CGCAGGGAUACCCGCG", "GCGCCCAUAGGGACGC")
-        self.assertGreater(distance, 0)
-
-    def test_distance_same(self):
-        distance = vienna_distance.get_distance("CGAUGCC", "CGAUGCC")
-        self.assertEqual(distance, 0)
-
-    def test_get_tRNA_sequence(self):
-        seq = vienna_distance.get_tRNA_sequence()
-        self.assertTrue(set(seq) <= set("ATCG"))
-
-    def test_get_distance_from_tRNA_sequence(self):
-        target = vienna_distance.get_tRNA_sequence()
-        distance_at_target = vienna_distance.get_distance_from_tRNA_sequence(
-            target)
-        self.assertEqual(distance_at_target, 0)
-
-    def test_fold(self):
-        seq = "ACTGAAATTGACCCTGTTAAAACTCGCTCGCTAGCTAGCTC"
-        struc = vienna_distance.fold(seq)
-        self.assertEqual(len(struc), len(seq))
+class TestModule(test_vienna_distance.TestModule):
+    module = cmd_vienna_distance
--- rna/test_vienna_distance.py
+++ rna/test_vienna_distance.py
@@ -1,29 +1,30 @@
 from unittest import TestCase as TC
-import vienna_distance as vienna_distance
+import vienna_distance
 
 
 class TestModule(TC):
+    module = vienna_distance
 
     def test_distance_unequal(self):
-        distance = vienna_distance.get_distance(
+        distance = self.module.get_distance(
             "CGCAGGGAUACCCGCG", "GCGCCCAUAGGGACGC")
         self.assertGreater(distance, 0)
 
     def test_distance_same(self):
-        distance = vienna_distance.get_distance("CGAUGCC", "CGAUGCC")
+        distance = self.module.get_distance("CGAUGCC", "CGAUGCC")
         self.assertEqual(distance, 0)
 
     def test_get_tRNA_sequence(self):
-        seq = vienna_distance.get_tRNA_sequence()
+        seq = self.module.get_tRNA_sequence()
         self.assertTrue(set(seq) <= set("ATCG"))
 
     def test_get_distance_from_tRNA_sequence(self):
-        target = vienna_distance.get_tRNA_sequence()
-        distance_at_target = vienna_distance.get_distance_from_tRNA_sequence(
+        target = self.module.get_tRNA_sequence()
+        distance_at_target = self.module.get_distance_from_tRNA_sequence(
             target)
         self.assertEqual(distance_at_target, 0)
 
     def test_fold(self):
         seq = "ACTGAAATTGACCCTGTTAAAACTCGCTCGCTAGCTAGCTC"
-        struc = vienna_distance.fold(seq)
+        struc = self.module.fold(seq)
         self.assertEqual(len(struc), len(seq))
