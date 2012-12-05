3279acf418b3ffb607cd9660d0c0089afa4919d8
Joshua Nahum <josh@nahum.us>
2012-12-05 15:28:09 -0800

    Added remove and choose functions

--- utility/fitness_tree.py
+++ utility/fitness_tree.py
@@ -4,6 +4,7 @@ in proportion to fitness (fecundity selection) and random death.
 
 Weighted (by fitness) binary tree.
 """
+import random
 
 class Node(object):
     """
@@ -24,8 +25,7 @@ class Node(object):
         self.calibrate_totals()
 
     def calibrate_totals(self):
-        if self.left is None and self.right is None:
-            assert self.leaf_contents is not None
+        if self.leaf_contents is not None:
             self.total_fitness = self.leaf_contents.fitness
             self.total_leaves = 1
         else:
@@ -38,13 +38,104 @@ class Node(object):
                 self.total_fitness += self.right.total_fitness
                 self.total_leaves += self.right.total_leaves
 
+def choose_leaf_by_fitness(tree):
+    """
+    Returns the contents of a leaf (an org) choosen randomly,
+    in proportion to fitness.
+    """
+    if tree_is_empty(tree):
+        raise ValueError("Can't choose from empty tree.")
+    if tree.leaf_contents is not None:
+        return tree.leaf_contents
+    if tree.left is not None and tree.right is not None:
+        if should_choose_left_by_fitness(tree):
+            return choose_leaf_by_fitness(tree.left)
+        else:
+            return choose_leaf_by_fitness(tree.right)
+
+def should_choose_left_by_fitness(tree):
+    """
+    Return True randomly in proportion to the fitness
+    of the leaves.
+    """
+    return should_choose_first_weighted(
+            tree.left.total_fitness, tree.right.total_fitness)
+
+def should_choose_left_by_number_of_leaves(tree):
+    """
+    Return True randomly in proportion to the number of leaves (uniformly).
+    """
+    return should_choose_first_weighted(
+            tree.left.total_leaves, tree.right.total_leaves)
+
+def should_choose_first_weighted(weight_a, weight_b):
+    """
+    Returns True randomly in proportion to the weight of a relative to b.
+    """
+    return random.random() < (weight_a / (weight_a + weight_b))
+
+
+def remove_leaf_uniformly(tree):
+    """
+    Removes a leaf from the tree randomly, and not with respect to fitness.
+    Every leaf has an equal chance of being removed.
+    Returns the tree and the org removed.
+    """
+    if tree_is_empty(tree):
+        raise ValueError("Can't remove from empty tree.")
+    if tree.leaf_contents is not None:
+        org, tree.leaf_contents = tree.leaf_contents, None
+        return tree, org
+    if tree.left is not None and tree.right is not None:
+        if should_choose_left_by_number_of_leaves(tree):
+            new_left, removed_org = remove_leaf_uniformly(tree.left)
+            if tree_is_empty(new_left):
+                tree.left = None
+            else:
+                tree.left = new_left
+        else:
+            new_right, removed_org = remove_leaf_uniformly(tree.right)
+            if tree_is_empty(new_right):
+                tree.right = None
+            else:
+                tree.right = new_right
+        tree.calibrate_totals()
+        return tree, removed_org
+
+    if tree.left is not None or tree.right is not None:
+        if tree.left is not None:
+            subtree = tree.left
+        else:
+            subtree = tree.right
+        return remove_leaf_uniformly(subtree)
+
+
+def tree_is_empty(tree):
+    return (tree.left is None and
+            tree.right is None and
+            tree.leaf_contents is None)
+
+def tree_to_list(tree):
+    """
+    Returns all of the leaf contents (orgs) as a list.
+    """
+    list_to_populate = []
+    if tree.left is not None:
+        list_to_populate += tree_to_list(tree.left)
+    if tree.leaf_contents is not None:
+        list_to_populate.append(tree.leaf_contents)
+    if tree.right is not None:
+        list_to_populate += tree_to_list(tree.right)
+    return list_to_populate
+
+
 def build_tree(orgs):
     """
-    Returns a tree from a list of organisms.
+    Returns a tree from an iterable of organisms.
     """
-    tree = singleton_tree(orgs[0])
-    for org in orgs[1:]:
-        tree = add_to_tree(tree, singleton_tree(org))
+    tree = empty_tree()
+    for org in orgs:
+        tree = add_to_tree(tree, org)
     return tree
 
 def singleton_tree(org):
@@ -53,14 +144,29 @@ def singleton_tree(org):
     """
     return Node(left=None, right=None, leaf_contents=org)
 
-def add_to_tree(tree_master, tree_other):
+def empty_tree():
+    """
+    Returns a tree lacking any orgs
+    """
+    return Node(left=None, right=None)
+
+def add_to_tree(tree, org):
+    """
+    Adds a single org to a tree.
+    """
+    return merge_trees(tree, singleton_tree(org))
+
+def merge_trees(tree_master, tree_other):
     """
     Adds the tree_other to the tree_master.
     """
 
     if tree_master.left is None and tree_master.right is None:
         # Tree master is leaf, convert to non-leaf.
-        return Node(left = tree_master, right = tree_other)
+        if tree_master.leaf_contents is None:
+            return tree_other
+        else:
+            return Node(left = tree_master, right = tree_other)
 
     if tree_master.left is None or tree_master.right is None:
         # Tree master is one sided, add other to right
@@ -70,9 +176,9 @@ def add_to_tree(tree_master, tree_other):
     else:
         # Tree master is full, add to lighter side
         if tree_master.left.total_fitness < tree_master.right.total_fitness:
-            tree_master.left = add_to_tree(tree_master.left, tree_other)
+            tree_master.left = merge_trees(tree_master.left, tree_other)
         else:
-            tree_master.right = add_to_tree(tree_master.right, tree_other)
+            tree_master.right = merge_trees(tree_master.right, tree_other)
     tree_master.calibrate_totals()
     return tree_master
 
--- utility/test_fitness_tree.py
+++ utility/test_fitness_tree.py
@@ -10,15 +10,52 @@ class MockOrganism(object):
     def __eq__(self, other):
         return self.identifier == other.identifier
 
+    def __hash__(self):
+        return hash(self.identifier)
+
     def __ne__(self, other):
         return not self == other
 
 class TestModule(TC):
 
-    def test_build_tree(self):
-        orgs = [MockOrganism(1, "a"),
+    def setUp(self):
+        self.orgs = [MockOrganism(1, "a"),
                 MockOrganism(2, "b"),
                 MockOrganism(3, "c")]
-        tree = build_tree(orgs)
-        self.assertEqual(tree.total_fitness, 6)
-        self.assertEqual(tree.total_leaves, 3)
+        self.tree = build_tree(self.orgs)
+
+    def test_build_tree(self):
+        self.assertEqual(self.tree.total_fitness, 6)
+        self.assertEqual(self.tree.total_leaves, 3)
+
+    def test_tree_to_list(self):
+        list_of_orgs = tree_to_list(self.tree)
+        self.assertEqual(set(list_of_orgs), set(self.orgs))
+
+    def test_add_to_tree(self):
+        org_added = MockOrganism(4, "d")
+        new_tree = add_to_tree(self.tree, org_added)
+        new_list = tree_to_list(new_tree)
+
+        self.assertEqual(set(new_list), set(self.orgs + [org_added]))
+        self.assertEqual(self.tree.total_fitness, 10)
+        self.assertEqual(self.tree.total_leaves, 4)
+
+    def test_remove_leaf_uniformly_all(self):
+        removed_orgs = []
+        tree = self.tree
+        for _ in range(len(self.orgs)):
+            tree, org = remove_leaf_uniformly(tree)
+            removed_orgs.append(org)
+        self.assertEqual([], tree_to_list(self.tree))
+        self.assertEqual(set(removed_orgs), set(self.orgs))
+
+    def test_remove_leaf_uniformly_exception(self):
+        for _ in range(len(self.orgs)):
+            self.tree, _ = remove_leaf_uniformly(self.tree)
+        with self.assertRaises(ValueError):
+            print(remove_leaf_uniformly(self.tree))
+
+    def test_choose_leaf_by_fitness(self):
+        org = choose_leaf_by_fitness(self.tree)
+        self.assertIn(org, self.orgs)
