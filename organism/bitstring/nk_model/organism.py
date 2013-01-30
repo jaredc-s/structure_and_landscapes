74972905322bb6ef9b5bf3ccd3894e4401aa4883
Joshua Nahum <josh@nahum.us>
2013-01-29 19:36:49 -0800

    completed revised tests for nk model org

--- organism/bitstring/nk_model/organism.py
+++ organism/bitstring/nk_model/organism.py
@@ -10,5 +10,12 @@ class Organism(BOrg):
         if not hasattr(self, "nk_model") or not isinstance(self.nk_model, NKModelSimple):
             raise ValueError("NK Organisms need a nk_model")
 
+
+    def mutate(self):
+        return type(self)(
+            value=self._mutated_value(),
+            parent_id=self.self_id,
+            nk_model=self.nk_model)
+
     def _evaluate_fitness(self):
         return self.nk_model.calculate_fitness(self.value)
--- organism/bitstring/nk_model/test_organism.py
+++ organism/bitstring/nk_model/test_organism.py
@@ -9,13 +9,20 @@ import random
 class TestOrganism(TC):
     def setUp(self):
         self.value = bitstring.Bitstring("100")
-        self.lookup = [{0:1, 1:0.5}, {0:0.2, 1:0.4}, {0:0.1, 1:0.8}]
+        lookup = [{0:1, 1:0.5}, {0:0.2, 1:0.4}, {0:0.1, 1:0.8}]
         deps = [[0], [1], [2]]
-        self.model = nk_model.NKModelSimple(deps, self.lookup)
-        self.org = Organism(self.value, nk_model=self.model)
+        model = nk_model.NKModelSimple(deps, lookup)
+        self.org = Organism(self.value, nk_model=model)
 
     def test_init(self):
         self.assertEqual(self.org.value, self.value)
+
+    def test_mutate(self):
+        self.assertNotEqual(self.org, self.org.mutate())
+
+    def test_without_nk_model(self):
+        with self.assertRaises(ValueError):
+            Organism(self.value)
 
     def test_fitness(self):
         expected_fit = (1 + 0.2 + 0.8) / float(3)
